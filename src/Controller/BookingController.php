4fe480e898bc9ccf47f0ae96110f24dab62ad366
Nithin Reddy <nithin@uniathena.com>
2025-04-15 18:54:57 +0530

    Booking API validations added.

--- src/Controller/BookingController.php
+++ src/Controller/BookingController.php
@@ -2,12 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Attendee;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Booking;
+use App\Entity\Event;
+use App\Entity\Users;
 
 #[Route('/api', name: 'api_')]
 final class BookingController extends AbstractController
@@ -17,6 +20,28 @@ final class BookingController extends AbstractController
     {
         $post_data = json_decode($request->getContent(), true);
         $now = new \DateTime();
+
+        // Validations.
+        $find_event = $entityManager->getRepository(Event::class)->findOneBy([
+            'id' => $post_data['event_id'],
+        ]);
+        if (!$find_event) {
+            return $this->json('Event Id does not match.', 404);
+        }
+
+        $find_attendee = $entityManager->getRepository(Attendee::class)->findOneBy([
+            'id' => $post_data['attendee_id'],
+        ]);
+        if (!$find_attendee) {
+            return $this->json('Attendee Id does not match.', 404);
+        }
+
+        $find_user = $entityManager->getRepository(Users::class)->findOneBy([
+            'id' => $post_data['user_id'],
+        ]);
+        if (!$find_user) {
+            return $this->json('User Id does not match.', 404);
+        }
 
         $find_booking = $entityManager->getRepository(Booking::class)->findOneBy([
             'event_id' => $post_data['event_id'],
