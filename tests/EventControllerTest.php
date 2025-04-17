428e02ff3df09466e4e6b1ba668b00f9a4e34d8d
Nithin Reddy <nithin@uniathena.com>
2025-04-17 19:30:30 +0530

    Test cases included.

--- tests/EventControllerTest.php
+++ tests/EventControllerTest.php
@@ -5,20 +5,139 @@ namespace App\Tests\Controller;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class EventControllerTest extends WebTestCase {
+
+    /**
+     * Create event test.
+     */
     public function testCreateEvent(): void {
         $client = static::createClient();
-
         $data = [
-            "name" => "Test event 101",
-            "description" => "Test description 100",
+            "name" => "Test event",
+            "description" => "Test description",
             "location" => "Australia",
-            "start_date" => "29-04-2025 09:25:00",
-            "end_date" => "30-04-2025 20:25:00",
+            "start_date" => date("Y-m-d h:i:s"),
+            "end_date" => date("Y-m-d h:i:s", strtotime('tomorrow')),
+            "status" => 1,
+            "created_by" => $this->createUser($client),
+            "max_attendees" => 100
+        ];
+        $result = $this->createEvent($data, $client);
+        $this->assertArrayHasKey('id', $result);
+        $this->assertEquals($data['name'], $result['name']);
+        $this->assertEquals($data['location'], $result['location']);
+    }
+
+    /**
+     * Update event test.
+     */
+    public function testUpdateEvent(): void {
+        $client = static::createClient();
+        $data = [
+            "name" => "Test event",
+            "description" => "Test description",
+            "location" => "Australia",
+            "start_date" => date("Y-m-d h:i:s", strtotime('+5 day')),
+            "end_date" => date("Y-m-d h:i:s", strtotime('+6 day')),
+            "status" => 1,
+            "created_by" => $this->createUser($client),
+            "max_attendees" => 100
+        ];
+        $event = $this->createEvent($data, $client);
+        $id = $event['id'];
+
+        $modified_data = [
+            "name" => "Test event Modified",
+            "description" => "Test description Modified",
+            "location" => "Australia",
+            "start_date" => date("Y-m-d h:i:s", strtotime('+5 day')),
+            "end_date" => date("Y-m-d h:i:s", strtotime('+6 day')),
+            "status" => 1,
+            "created_by" => $this->createUser($client),
+            "max_attendees" => 100
+        ];
+
+        $client->request(
+            'PUT',
+            '/api/event/' . $id,
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json'],
+            json_encode($modified_data)
+        );
+        $result = json_decode($client->getResponse()->getContent(), true);
+
+        $this->assertArrayHasKey('id', $result);
+        $this->assertStringContainsStringIgnoringCase('modified', $result['name']);
+        $this->assertStringContainsStringIgnoringCase('modified', $result['description']);
+    }
+
+    /**
+     * Get event test.
+     */
+    public function testGetEvent(): void {
+        $client = static::createClient();
+        $data = [
+            "name" => "Test event",
+            "description" => "Test description",
+            "location" => "Australia",
+            "start_date" => date("Y-m-d h:i:s", strtotime('+1 day')),
+            "end_date" => date("Y-m-d h:i:s", strtotime('+2 day')),
             "status" => 1,
             "created_by" => 1,
             "max_attendees" => 100
         ];
+        $event = $this->createEvent($data, $client);
+        $id = $event['id'];
 
+        $client->request(
+            'GET',
+            '/api/event/' . $id,
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json'],
+        );
+
+        $result = json_decode($client->getResponse()->getContent(), true);
+        $this->assertArrayHasKey('id', $result);
+        $this->assertEquals($data['name'], $result['name']);
+        $this->assertEquals($data['location'], $result['location']);
+    }
+
+    /**
+     * Delete event test.
+     */
+    public function testDeleteEvent(): void {
+        $client = static::createClient();
+        $data = [
+            "name" => "Test event",
+            "description" => "Test description",
+            "location" => "Australia",
+            "start_date" => date("Y-m-d h:i:s", strtotime('+3 day')),
+            "end_date" => date("Y-m-d h:i:s", strtotime('+4 day')),
+            "status" => 1,
+            "created_by" => 1,
+            "max_attendees" => 100
+        ];
+        $event = $this->createEvent($data, $client);
+        $id = $event['id'];
+
+        $message = 'Deleted a event successfully with id ' . $id;
+        $client->request(
+            'DELETE',
+            '/api/event/' . $id,
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json'],
+        );
+
+        $result = json_decode($client->getResponse()->getContent(), true);
+        $this->assertEquals($message, $result);
+    }
+
+    /**
+     * Create actual event.
+     */
+    public function createEvent($data, $client): array {
         $client->request(
             'POST',
             '/api/event',
@@ -27,12 +146,32 @@ class EventControllerTest extends WebTestCase {
             ['CONTENT_TYPE' => 'application/json'],
             json_encode($data)
         );
-
-        $responseData = json_decode($client->getResponse()->getContent(), true);
-        error_log(print_r($responseData, true));
-
-        $this->assertArrayHasKey('id', $responseData);
-        $this->assertEquals($data['name'], $responseData['name']);
-        $this->assertEquals($data['location'], $responseData['location']);
+        $event = json_decode($client->getResponse()->getContent(), true);
+        return $event;
     }
+
+    /**
+     * Create actual user.
+     */
+    public function createUser($client): int
+    {
+        $name = "user" . rand(1, 999);
+        $email = "user" . rand(9999, 9999999) . 'test@email.com';
+        $data = [
+            "name" => $name,
+            "email" => $email,
+        ];
+        $client->request(
+            'POST',
+            '/api/user',
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json'],
+            json_encode($data)
+        );
+
+        $result = json_decode($client->getResponse()->getContent(), true);
+        return $result['id'];
+    }
+
 }
---
+++ tests/UserControllerTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class UserControllerTest extends WebTestCase {
+
+    public function testUserCreate(): void
+    {
+        $client = static::createClient();
+
+        $name = "user" . rand(1, 999);
+        $email = "user" . rand(9999, 9999999) . 'test@email.com';
+        $data = [
+            "name" => $name,
+            "email" => $email,
+        ];
+
+        $client->request(
+            'POST',
+            '/api/user',
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json'],
+            json_encode($data)
+        );
+
+        $result = json_decode($client->getResponse()->getContent(), true);
+        $this->assertArrayHasKey('id', $result);
+        $this->assertEquals($data['name'], $result['username']);
+        $this->assertEquals($data['email'], $result['email']);
+    }
+}
